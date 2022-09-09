211d350c1d5be409717c44992cfd83ec903e7a88
Thabo <tgsmdletshe@gmail.com>
2022-09-08 17:10:15 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
-
-
+/**
+ * main - Prin the string
+ *
+ *Description: use main function
+ * return: 0 (successful)
+ */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
