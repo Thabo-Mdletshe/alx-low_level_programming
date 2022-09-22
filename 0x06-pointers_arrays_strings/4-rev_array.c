9a1013683001c3afdd1969ff41d4b8bb86b589f3
Thabo <tgsmdletshe@gmail.com>
2022-09-22 16:35:10 -0700

    3-strcmp.c and 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: An aray of integers
+ * @n: Number of elements to swap
+ *
+ * Return: empty
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		P++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
