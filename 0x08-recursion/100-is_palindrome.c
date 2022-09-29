34d40691abf437983742e88dce372d312ae90cac
Thabo <tgsmdletshe@gmail.com>
2022-09-28 21:07:45 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: first input
+ * @s2: second input
+ * Return: 1 if strings are identical and 0 otherwiae
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wldcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
