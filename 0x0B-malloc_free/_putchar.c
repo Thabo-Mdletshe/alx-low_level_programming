3ed49d44c0dc369e84afcd817db1efd594b3c738
Thabo-Mdletshe <tgsmdletshe@gmail.com>
2022-10-04 23:10:33 -0700

    task 0 to 2

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character  c
+ * @c: The character to print
+ *
+ * Return: 1 (success)
+ * On error, -1 returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1))
+}
